29fd82fb069bde1d22f4812f792426b573a0b16a
maikalangi <maikalangi@gmail.com>
2022-05-18 13:43:33 -0700

    commits routes to index, show, and new pages

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const budget = require('./models/data')
+const res = require('express/lib/response');
+const budget = require('./models/budget')
 
 const app = express();
 
 const port = 3002;
+
+// Index
+app.get('/budgets/', (req, res)=>{
+    res.render('index.ejs', { budget });
+});
+
+// New
+app.get('/budgets/new/', (req, res)=>{
+
+})
+
+// Create
+
+// Show
+app.get('/budgets/:id', (req, res)=>{
+    res.render('show.ejs', {
+        budget: budget[req.params.id]
+    });
+});
 
 app.listen(port, ()=>{
     console.log('listening to port ', port);
