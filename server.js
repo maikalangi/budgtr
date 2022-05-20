26017e6f32d468d1ba0653a8ab3b1050e70a9c04
maikalangi <maikalangi@gmail.com>
2022-05-20 12:28:27 -0700

    adds node modules for body parser

--- server.js
+++ server.js
@@ -1,37 +1,47 @@
+// DEPENDENCIES
 const express = require('express');
 const res = require('express/lib/response');
-const budget = require('./models/budget')
+const budget = require('./models/budget');
+const bodyParser = require('body-parser');
 
+// APP INITIALIZER
 const app = express();
 
+// APP CONFIGURATIONS
 const port = 3002;
-// Middleware
-app.use(express.static('public'));
 
-// Index
+// MIDDLEWARE
+app.use(express.static('public'));
+// app.use(bodyParser(''));
+
+// ===============ROUTES===================
+
+// INDEX
+// 1
 app.get('/', (req, res)=>{
     res.send('Hello World');
 });
-
+// 2
 app.get('/budget/', (req, res)=>{
     res.render('index.ejs', { budget });
 });
 
-// New
+// NEW
 app.get('/budget/new/', (req, res)=>{
     res.render('new.ejs');
 });
 
-// Create
+// CREATE
 
-// Show
+
+// SHOW
 app.get('/budget/:id', (req, res)=>{
     res.render('show.ejs', {
         budget: budget[req.params.id]
     });
 });
 
-
+// ROUTE LISTENER
 app.listen(port, ()=>{
     console.log('listening to port ', port);
 });
