c99619f04416a26f6a623c8ebb66871eaf5b870a
maikalangi <maikalangi@gmail.com>
2022-05-22 10:47:25 -0700

    fixes route names for all pages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const res = require('express/lib/response');
 const budget = require('./models/budget');
+const methodOverride = require('method-override');
 
 // APP INITIALIZER
 const app = express();
@@ -12,7 +13,7 @@ const port = 3002;
 // MIDDLEWARE
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(methodOverride('_method'));
 // ===============ROUTES===================
 
 // INDEX
@@ -22,7 +23,10 @@ app.get('/', (req, res)=>{
 });
 // 2
 app.get('/budgets/', (req, res)=>{
-    res.render('index.ejs', { budget });
+    res.render('index.ejs', { 
+        budget,
+        index: req.params.id,
+    });
 });
 
 // NEW
@@ -39,8 +43,14 @@ app.post('/budgets/', (req, res)=>{
 // SHOW
 app.get('/budgets/:id', (req, res)=>{
     res.render('show.ejs', {
-        budget: budget[req.params.id]
+        budget: budget[req.params.id],
     });
+});
+
+// DELETE
+app.delete('/budgets/:id/', (req, res)=>{
+    budget.splice(req.params.id, 1);
+    res.redirect('/budgets/');
 });
 
 // ROUTE LISTENER
